9be9479dba114e633a3a093f6d059c1dfc006bd2
Savata <sava.ivanov.9@gmail.com>
2017-08-18 01:10:21 +0300

    Permission model added

--- Server/CompanyManagementSystem/CompanyManagementSystem.DbModels/Position.cs
+++ Server/CompanyManagementSystem/CompanyManagementSystem.DbModels/Position.cs
@@ -7,10 +7,12 @@
     public class Position
     {
         private ICollection<Employee> employees;
+        private ICollection<Permission> permissions;
 
         public Position()
         {
             this.employees = new HashSet<Employee>();
+            this.permissions = new HashSet<Permission>();
         }
 
         [Key]
@@ -25,6 +27,12 @@
         {
             get => this.employees;
             set => this.employees = value;
+        }
+
+        public virtual ICollection<Permission> Permissions
+        {
+            get => this.permissions;
+            set => this.permissions = value;
         }
     }
 }

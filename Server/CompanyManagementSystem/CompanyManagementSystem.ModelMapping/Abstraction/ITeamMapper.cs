a066463a2feadc933986f6cb54de2408356a3e42
Savata <sava.ivanov.9@gmail.com>
2017-08-19 14:40:24 +0300

    team mapper interface added

---
+++ Server/CompanyManagementSystem/CompanyManagementSystem.ModelMapping/Abstraction/ITeamMapper.cs
@@ -0,0 +1,12 @@
+﻿namespace CompanyManagementSystem.ModelMapping.Abstraction
+{
+    using DataTransferModels.Team;
+    using DbModels;
+    using System;
+    using System.Linq.Expressions;
+
+    public interface ITeamMapper
+    {
+        Expression<Func<Team, TeamReadModel>> MapDbModelToDtm();
+    }
+}

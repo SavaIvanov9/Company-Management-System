17b0e1ce70b27ff16f18b3f257f8b2814d07ae89
Savata <sava.ivanov.9@gmail.com>
2017-09-02 02:26:09 +0300

    fixes

--- New folder/CompanyManagementSystem.WebClient/src/app/login/components/login-form.component.ts
+++ New folder/CompanyManagementSystem.WebClient/src/app/login/components/login-form.component.ts
@@ -2,15 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { LoginService } from '../services/login.service';
 
-// import { WinAuth } from '../models/win-auth.model';
-// import { Credentials } from '../models/credentials.model';
-// import { Cookie } from '../models/cookie.model';
-
-// import {CookieService, CookieOptionsArgs} from 'angular2-cookie/core';
-// import { Router } from '@angular/router';
-// import { CryptoManager } from '../../core/services/crypto-manager';
-// import { CookieKeys } from '../../shared/constants/constants';
-
 @Component({
     selector: 'app-login-form',
     templateUrl: './login-form.component.html',
@@ -19,81 +10,11 @@ import { LoginService } from '../services/login.service';
 
 export class LoginFormComponent implements OnInit {
 
-    // private winAuth: WinAuth;
-    // private credentials: Credentials;
-    // private cookie: Cookie;
-    // private cookieOptionsArgs: CookieOptionsArgs = {};
-    private isLogging: boolean;
-    private authLoaded: boolean;
-    private wrongUserPass: boolean;
-    private isDomainAccount: boolean;
-
-    constructor(
-        // private loginService: LoginService,
-        // private cookieService: CookieService,
-        // private router: Router,
-        // private cryptoManager: CryptoManager
-    ) {
-        // this.credentials = new Credentials();
-    }
-
-    GetWinAuth() {
-        // this.loginService.getWinAuth()
-        //     .map((winAuth: WinAuth) => this.winAuth = winAuth)
-        //     .subscribe(() => this.authLoaded = true, () => this.authLoaded = true);
-    }
-
-    GetToken() {
-        // this.isLogging = true;
-        // if (this.winAuth) {
-        //     this.credentials.UserName = this.winAuth.Name;
-        //     this.credentials.Password = this.winAuth.SIDorPass;
-        // }
-        // this.loginService.GetToken(this.credentials)
-        //     .catch(error => this.credentials = undefined)
-        //     .map((cookie: Cookie) => this.cookie = cookie)
-        //     .map(() => {
-        //         this.cookieService.put(CookieKeys.Token, this.cookie.access_token, this.cookieOptionsArgs);
-        //         this.cookieService.put(CookieKeys.Username, this.cryptoManager.Encrypt(this.cookie.userName), this.cookieOptionsArgs);
-        //         this.cookieService.put(CookieKeys.Position, this.cryptoManager.Encrypt(this.cookie.position), this.cookieOptionsArgs);
-        //         this.cookieService.put(CookieKeys.Permissions, this.cryptoManager.Encrypt(this.cookie.permissions), this.cookieOptionsArgs);
-        //     })
-        //     .subscribe(data => console.log(data),
-        //     error => this.WrongCredentials(error),
-        //     () => this.router.navigate(['/about']));
-    }
-
-    LoginWithOtherAccount() {
-        //this.winAuth = undefined;
-        this.isDomainAccount = true;
-    }
-
-    WrongCredentials(error) {
-        console.log(error)
-        this.isLogging = false;
-        this.wrongUserPass = true;
-        //this.credentials = new Credentials();
-    }
-
-    Back() {
-        this.GetWinAuth();
-        this.wrongUserPass = false;
-    }
-
-    keyDownFunction(event) {
-        if (event.keyCode === 13) {
-            this.GetToken()
-        }
+    constructor() {
     }
 
     ngOnInit() {
         console.log('Login form')
-        this.GetWinAuth();
-        //this.cookieOptionsArgs.expires = new Date(new Date().getTime() + 180 * 60000);
-        this.isLogging = false;
-        this.authLoaded = false;
-        this.wrongUserPass = false;
-        this.isDomainAccount = false;
     }
 }
 
--- New folder/CompanyManagementSystem.WebClient/src/app/login/services/login.service.ts
+++ New folder/CompanyManagementSystem.WebClient/src/app/login/services/login.service.ts
@@ -1,68 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { Response, Headers, URLSearchParams } from '@angular/http';
-
-import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
-
-// import { TokenUrl, DomainAuthUrl } from '../../shared/constants/constants';
-// import { ErrorHandler } from '../../shared/handlers/error-handler';
-
-// import { WinAuth } from '../models/win-auth.model';
-// import { Cookie } from '../models/cookie.model';
-// import { Credentials } from '../models/credentials.model';
-// import { User } from '../models/user.model';
-
-import 'rxjs/add/observable/empty';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/catch';
-
-// import { CookieService, CookieOptionsArgs } from 'angular2-cookie/core';
-// import { HttpClient } from '../../core/services/http-client'
-
 @Injectable()
 export class LoginService {
 
-    // private tokenUrl: string = TokenUrl + 'Token';
-    // private identityURL: string = TokenUrl + 'api/account/identity';
-    // private logoutUrl: string = TokenUrl + 'api/account/logout';
-    // private domainAuthUrl: string = DomainAuthUrl + 'api/auth';
-
-    // private winAuth: Observable<WinAuth>;
-    // private token: string;
-    // private username: Observable<string>;
-
-    // constructor(private httpClient: HttpClient, private errorHandler: ErrorHandler, private cookieService: CookieService) {
-    // }
-
-    // getWinAuth(): Observable<WinAuth> {
-    //     this.winAuth = this.httpClient.getWithOptions(this.domainAuthUrl, { withCredentials: true })
-    //         .map((res: Response) => res.json());
-
-    //     return this.winAuth;
-    // }
-
-    // GetToken(credentials: Credentials): Observable<Cookie> {
-    //     let params = new URLSearchParams();
-    //     let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
-
-    //     params.set('grant_type', 'password');
-    //     params.set('UserName', credentials.UserName);
-    //     params.set('Password', credentials.Password);
-
-    //     return this.httpClient.postWithOptions(this.tokenUrl, params, { headers: headers })
-    //         .map((res: Response) => {
-    //             return res.json();
-    //         })
-    //         .catch(error => {
-    //             if (error.status === 400) {
-    //                 return Observable.throw('GRESHKA!');
-    //             }
-    //         });
-    // }
-
-    LogOut() {
-        //return this.httpClient.post(this.logoutUrl, null);
-    }
 }
